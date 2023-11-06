f4e5502144b5c5585ab07fea52887447a6155245
Wayne Piekarski <wayne@tinmith.net>
2023-11-06 09:00:06 -0800

    Specialized diff to use for getting changed hex addresses to use with fs4-dosbox debugger, cmp does not have a suitable format

---
+++ BattleTech/Hacks/hexdiff.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <cstdlib>
+#include <ctype.h>
+#include <vector>
+#include <string.h>
+
+// Diff two files but using hex addresses and values in hex as well as how much the delta was
+bool flag_no_graphics = false;
+
+int main (int argc, char* _argv[]) {
+  std::vector<char*> argv;
+  for(int i = 0; i < argc; i++) {
+    if (!strcmp(_argv[i], "--no-graphics")) {
+      flag_no_graphics = true;
+      fprintf(stderr, "Will stop before A000:0000 graphics memory\n");
+    } else {
+      argv.push_back(_argv[i]);
+    }
+  }
+  if (argc != 3) { fprintf(stderr, "Two files required"); exit(1); }
+  FILE *fpa = fopen(argv[1], "r");
+  if (fpa == nullptr) { fprintf(stderr, "File %s not found", argv[1]); exit(1); }
+  FILE *fpb = fopen(argv[2], "r");
+  if (fpb == nullptr) { fprintf(stderr, "File %s not found", argv[2]); exit(1); }
+  printf("%s %s %s\n", argv[0], argv[1], argv[2]);
+
+  size_t ofs = 0;
+  while(1) {
+    unsigned char a;
+    int aa = fread(&a, 1, 1, fpa);
+    unsigned char b;
+    int bb = fread(&b, 1, 1, fpb);
+    if ((aa != 1) && (bb != 1)) {
+      fprintf(stderr, "Finished diff\n");
+      exit(0);
+    }
+    if (aa != 1) {
+      fprintf(stderr, "Early EOF on %s\n", argv[1]);
+      exit(1);
+    }
+    if (bb != 1) {
+      fprintf(stderr, "Early EOF on %s\n", argv[2]);
+      exit(1);
+    }
+
+    if (a == b) {
+      continue;
+    }
+
+    printf("ofs=%.5zX src=%.2X dst=%.2X diff=%+d\n", ofs, a, b, (signed char)(a-b));
+    ofs++;
+    if ((flag_no_graphics) && (ofs >= 0xA0000)) {
+      fprintf(stderr, "Stopping before A000:0000 framebuffer\n");
+      exit(0);
+    }
+  }
+}
