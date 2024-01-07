2d6e0c4fe49fd1c5296aa88f7bed6ed16424e458
Wayne Piekarski <wayne@tinmith.net>
2024-01-06 23:56:46 -0800

    Reduce the range of writes and now the map is fully revealed without corrupting anything

--- BattleTech/Hacks/show-map-savegame.sh
+++ BattleTech/Hacks/show-map-savegame.sh
@@ -2,6 +2,4 @@
 
 echo "Enabling map visibility in $1"
 set -xv
-echo "TODO: Seems to remove the Chameleon mech that I had"
-`dirname $0`/setbytes $1 0010 0CFC FF
-`dirname $0`/setbytes $1 0E40 0F44 FF
+`dirname $0`/setbytes $1 0200 0CFC FF
