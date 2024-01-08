bd4e7083efe9d9be9a1555fd2408480a3a46e1c3
Wayne Piekarski <wayne@tinmith.net>
2024-01-07 23:17:50 -0800

    Combine map tiles with cropping and offsets to produce a single map image

--- BattleTech/map-capture-raw/crop-screenshots.sh
+++ BattleTech/map-capture-raw/crop-screenshots.sh
@@ -9,10 +9,26 @@ Y=0
 # Sort in y-x order so the filenames are correctly arranged in the file explorer
 for each in `ls -1 btech_*.png`; do
   echo "Cropping ${each} to crop/map-y${Y}-x${X}.png"
-  convert -crop 320x192+0+0 ${each} crop/map-y${Y}-x${X}.png
+  # The map scroll has significant overlap, so need to crop or offset the images to fit
+  if [[ $Y == 0 ]]; then
+      CROPY=64
+  else
+      CROPY=128
+  fi
+  if [[ $X == 6 ]]; then
+      CROPX=320
+      OFSX=56
+  else
+      CROPX=128
+      OFSX=0
+  fi
+  convert -crop ${CROPX}x${CROPY}+${OFSX}+0 ${each} crop/map-y${Y}-x${X}.png
   let "X+=1"
   if [[ $X == 7 ]]; then
     X=0
     let "Y+=1"
   fi
 done
+
+# Combine into a single image
+montage -mode concatenate -tile 7x crop/map-*.png crop/map-combined.png
