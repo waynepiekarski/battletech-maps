ff2442e2d13ef3242708b27194c0b9f06ec77d1d
Wayne Piekarski <wayne@tinmith.net>
2024-01-24 01:08:04 -0800

    Draft support for loading all the PNGs into memory but hitting 64-bit limits and need to use 8-bit storage

--- BattleTech/Hacks/save2png.cpp
+++ BattleTech/Hacks/save2png.cpp
@@ -19,6 +19,15 @@ int main (int argc, char* argv[]) {
     fprintf(stderr, "Could not open input directory %s\n", argv[1]);
     exit(1);
   }
+
+  // Allocate an image big enough to store the entire universe
+  // Tiles are 16x16 and screenshots are 216x200
+  // Maximum coordinates are 0x0FFF,0x0FFF = 4096,4096
+  // In reality, X is limited to 0xF7F since this is the top-left corner
+  size_t universe_size = 3 * 4096*16 * 4096*16;
+  unsigned char* universe = (unsigned char*)malloc(universe_size);
+  memset(universe, 0xAA, universe_size);
+
   while ((dir = readdir(d)) != NULL) {
     if (strstr(dir->d_name, "save-y") == NULL)
       continue;
@@ -35,9 +44,13 @@ int main (int argc, char* argv[]) {
     xbuf[4] = '\0';
     int x = strtol(xbuf, NULL, 16);
     int y = strtol(ybuf, NULL, 16);
+    // Compute the actual top-left XY coordinates of this screenshot
+    int xp = x * 16;
+    int yp = y * 16;
+
     char path [4096];
     sprintf(path, "%s/%s", argv[1], dir->d_name);
-    printf("Input path %s with X=[%s]=0x%.3X Y=[%s]=0x%.3X\n", path, xbuf, x, ybuf, y);
+    printf("Input path %s with X=[%s]=0x%.3X Y=[%s]=0x%.3X --> (%d,%d)\n", path, xbuf, x, ybuf, y, xp, yp);
 
     // Decode the PNG
     unsigned char*rgb24 = nullptr;
@@ -48,12 +61,27 @@ int main (int argc, char* argv[]) {
       exit(1);
     }
 
-    // Tiles are 16x16 and screenshots are 216x200
     if ((w != 216) || (h != 200)) {
       fprintf (stderr, "Invalid dimensions %dx%d from %s\n", w, h, path);
     }
 
+    // Copy the PNG into the universe
+    unsigned char *srcptr = rgb24;
+    unsigned char *dstptr = universe + (yp*3) + xp;
+    for (int r = 0; r < 200; r++) {
+      memcpy(dstptr, srcptr, 216*3);
+      srcptr += 216*3;
+      dstptr += 4096*16*3;
+    }
   }
   closedir(d);
+
+  // Write out the universe
+  unsigned error = lodepng_encode24_file("universe.png", universe, 4096*16, 4096*16);
+  if (error != 0) {
+    fprintf(stderr, "Failed to write out universe.png\n");
+    exit(1);
+  }
+
   return 0;
 }
